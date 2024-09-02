acfda9b5866360e199e9a76182780b5cd685b44b
Praveendubey_03 <Praveendubey955@outlook.com>
2024-09-02 17:45:56 +0530

    ADDED DELETE BUTTON TO THE COMMENTS 1. write a destroy function in comments controller , 2. Create a route for that function, 3. Added a delete button in the home.ejs

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,7 +1,7 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = async function(req, res) {
+module.exports.create = async function (req, res) {
     try {
         // Find the post by ID
         const post = await Post.findById(req.body.post);
@@ -33,5 +33,41 @@ module.exports.create = async function(req, res) {
         // Handle errors
         console.error(err);
         return res.status(500).send('Server error');
+    }
+};
+
+
+
+module.exports.destroy = async function(req, res) {
+    try {
+        // Find the comment by ID
+        const comment = await Comment.findById(req.params.id).exec();
+        
+        if (!comment) {
+            // If the comment does not exist, redirect back
+            return res.redirect('back');
+        }
+
+        // Check if the current user is the owner of the comment
+        if (comment.user.toString() === req.user.id.toString()) {
+            // Extract the post ID from the comment
+            const postId = comment.post;
+
+            // Remove the comment ID from the post's comments array
+            await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }).exec();
+
+            // Delete the comment
+            await Comment.findByIdAndDelete(req.params.id).exec();
+
+            // Redirect back
+            return res.redirect('back');
+        } else {
+            // If the user is not the owner, redirect back
+            return res.redirect('back');
+        }
+    } catch (err) {
+        // Handle errors and respond appropriately
+        console.error(err);
+        return res.redirect('back');
     }
 };
