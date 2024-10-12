cd13f73ab5ed8d8560f044c11c76f3663fd5f56f
Praveendubey_03 <Praveendubey955@outlook.com>
2024-10-12 11:47:23 +0530

    added passport jwt

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,31 @@
+
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+
+
+module.exports.createSessions = async function (req, res) {
+
+    try{
+    let user = await User.findOne({email: req.body.email});
+
+    if(!user || user.password != req.body.password){
+        return res.json(422, {
+            message: "Invaild Username or Password"
+        });
+    }
+
+    return res.json(200, {
+        message: "Sign in sucessfully, here is your token, please take it",
+        data: {
+            token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '100000'})
+        }
+    });
+
+}catch(err){
+    console.log('******', err);
+    return res.json(500, {
+        message: "Internal Server Error"
+    })
+}
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const MongoStore = require('connect-mongo'); 
